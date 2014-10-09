30d650d3e4ea17e14f5356f552f01dd987516065
Christian M. Jensen <christian@cmjscripter.net>
2014-10-09 16:32:45 +0200

    move copy files list
    move copy files list into own file for use in more places

--- _ins/Installer.php
+++ _ins/Installer.php
@@ -27,55 +27,8 @@ class Installer {
      * list of files to copy
      * @var array 
      */
-    static $files_copy = array(
-        'interface' => array(
-            'lib/classes/sogo_helper.inc.php',
-            //* #ADMIN#
-            //* admin
-            'web/admin/sogo_conifg_del.php',
-            'web/admin/sogo_conifg_edit.php',
-            'web/admin/sogo_conifg_list.php',
-            'web/admin/sogo_conifg_rebuild.php',
-            'web/admin/sogo_domains_del.php',
-            'web/admin/sogo_domains_edit.php',
-            'web/admin/sogo_domains_list.php',
-            //* form
-            'web/admin/form/sogo_config.tform.php',
-            'web/admin/form/sogo_domains.tform.php',
-            //* lng
-            'web/admin/lib/lang/en_sogo_config.lng',
-            'web/admin/lib/lang/en_sogo_conifg_list.lng',
-            'web/admin/lib/lang/en_sogo_domains.lng',
-            'web/admin/lib/lang/en_sogo_domains_list.lng',
-            //* menu
-            'web/admin/lib/menu.d/sogo.menu.php',
-            //* list
-            'web/admin/list/sogo_domains.list.php',
-            'web/admin/list/sogo_server.list.php',
-            //* templates
-            'web/admin/templates/sogo_config_custom_edit.htm',
-            'web/admin/templates/sogo_config_domain_edit.htm',
-            'web/admin/templates/sogo_config_edit.htm',
-            'web/admin/templates/sogo_config_user_edit.htm',
-            'web/admin/templates/sogo_conifg_list.htm',
-            'web/admin/templates/sogo_domains_custom_edit.htm',
-            'web/admin/templates/sogo_domains_domain_edit.htm',
-            'web/admin/templates/sogo_domains_list.htm',
-            'web/admin/templates/sogo_domains_user_edit.htm',
-        //* /#ADMIN#
-        //* #MAIL#
-        //* currently empty
-        //* /#MAIL#
-        ),
-        'server' => array(
-            'conf/sogo_domain.master',
-            'lib/config.inc.local.sogo-sample.php',
-            'lib/classes/sogo_config.inc.php',
-            'lib/classes/sogo_helper.inc.php',
-            'mods-available/sogo_module.inc.php',
-            'plugins-available/sogo_plugin.inc.php',
-        ),
-    );
+    static public $files_copy = array();
+    
     static private $mysql_tables_ispc = "_ins/tables.sql";
 
     /** @var string location of ISPConfig home dir */
@@ -244,6 +197,11 @@ class Installer {
         } else {
             echo "Not installing server files, server folder not found" . PHP_EOL;
         }
+    }
+    
+    //* quick lazy alias
+    public static function copyFiles($index) {
+        $this->_copyFiles($index);
     }
 
     private function _copyFiles($index) {
---
+++ _ins/copy_files.php
@@ -0,0 +1,51 @@
+<?php
+
+$files_copy = array(
+    'interface' => array(
+        'lib/classes/sogo_helper.inc.php',
+        //* #ADMIN#
+        //* admin
+        'web/admin/sogo_conifg_del.php',
+        'web/admin/sogo_conifg_edit.php',
+        'web/admin/sogo_conifg_list.php',
+        'web/admin/sogo_conifg_rebuild.php',
+        'web/admin/sogo_domains_del.php',
+        'web/admin/sogo_domains_edit.php',
+        'web/admin/sogo_domains_list.php',
+        //* form
+        'web/admin/form/sogo_config.tform.php',
+        'web/admin/form/sogo_domains.tform.php',
+        //* lng
+        'web/admin/lib/lang/en_sogo_config.lng',
+        'web/admin/lib/lang/en_sogo_conifg_list.lng',
+        'web/admin/lib/lang/en_sogo_domains.lng',
+        'web/admin/lib/lang/en_sogo_domains_list.lng',
+        //* menu
+        'web/admin/lib/menu.d/sogo.menu.php',
+        //* list
+        'web/admin/list/sogo_domains.list.php',
+        'web/admin/list/sogo_server.list.php',
+        //* templates
+        'web/admin/templates/sogo_config_custom_edit.htm',
+        'web/admin/templates/sogo_config_domain_edit.htm',
+        'web/admin/templates/sogo_config_edit.htm',
+        'web/admin/templates/sogo_config_user_edit.htm',
+        'web/admin/templates/sogo_conifg_list.htm',
+        'web/admin/templates/sogo_domains_custom_edit.htm',
+        'web/admin/templates/sogo_domains_domain_edit.htm',
+        'web/admin/templates/sogo_domains_list.htm',
+        'web/admin/templates/sogo_domains_user_edit.htm',
+    //* /#ADMIN#
+    //* #MAIL#
+    //* currently empty
+    //* /#MAIL#
+    ),
+    'server' => array(
+        'conf/sogo_domain.master',
+        'lib/config.inc.local.sogo-sample.php',
+        'lib/classes/sogo_config.inc.php',
+        'lib/classes/sogo_helper.inc.php',
+        'mods-available/sogo_module.inc.php',
+        'plugins-available/sogo_plugin.inc.php',
+    ),
+);
