1556e151ae8f44e974b189ccff865c43e18ea032
Christian M. Jensen <christian@cmjscripter.net>
2014-11-06 08:22:05 +0100

    sync sogo_helper class for interface

--- interface/lib/classes/sogo_helper.inc.php
+++ interface/lib/classes/sogo_helper.inc.php
@@ -33,10 +33,10 @@ class sogo_helper {
      * get a list of domains name,id and server of domains with sogo configurations
      * @return sogo_domains_list[]
      */
-    public function listDomains() {
+    public function listDomains($authsql = "") {
         global $app;
         $list = array();
-        $result = $app->db->queryAllRecords('SELECT `sogo_id`, `domain_id`, `server_id`,`domain_name` FROM `sogo_domains`');
+        $result = $app->db->queryAllRecords('SELECT `sogo_id`, `domain_id`, `server_id`,`domain_name` FROM `sogo_domains`' . (!empty($authsql) ? ' WHERE (' . $authsql . ')' : ''));
         if ($result === FALSE)
             return $list;
         foreach ($result as $value) {
@@ -66,6 +66,7 @@ class sogo_helper {
         $result = $app->db->queryOneRecord('SELECT `sogo_id` FROM `sogo_domains` WHERE `domain_id`=' . intval($domain_id));
         return isset($result['sogo_id']) ? $result['sogo_id'] : 0;
     }
+
 //    
 //    /**
 //     * get a list of server name+id of server with sogo configurations
@@ -112,7 +113,12 @@ class sogo_helper {
     public function configExists($server_id) {
         global $app;
         $result = $app->db->queryOneRecord('SELECT `server_id` FROM `sogo_config` WHERE `server_id`=' . intval($server_id));
-        return (boolean) ($result['server_id'] == $server_id);
+        return (boolean) ($result['server_id'] == $server_id) && (isset($result['server_id']) && $result['server_id'] > 0);
+    }
+    public function configExistsByDomain($domain_id) {
+        global $app;
+        $result = $app->db->queryOneRecord('SELECT sc.`server_id`, sd.`domain_id` FROM `sogo_config` sc, `sogo_domains` sd WHERE sc.`server_id`=sd.`server_id` AND sd.`domain_id`=' . intval($domain_id));
+        return (boolean) ($result['domain_id'] == $domain_id) && (isset($result['server_id']) && $result['server_id'] > 0);
     }
 
     /**
@@ -124,6 +130,31 @@ class sogo_helper {
         global $app;
         $result = $app->db->queryOneRecord('SELECT `sogo_id` FROM `sogo_config` WHERE `server_id`=' . intval($server_id));
         return isset($result['sogo_id']) ? $result['sogo_id'] : 0;
+    }
+
+    /**
+     * gets the domain config fileds from the database<br>
+     * <pre><code>
+     * array(
+     *     COLUMN-NAME => array(
+     *         name => COLUMN-NAME,
+     *         default => Default value as set in db,
+     *     ),
+     * );
+     * </code></pre>
+     * @return array
+     */
+    public function getDomainConfigFields() {
+        global $app;
+        $domains_columns = $app->db->queryAllRecords("SHOW COLUMNS FROM `sogo_domains`");
+        $ret = array();
+        foreach ($domains_columns as $value) {
+            if ($value['Field'] == "domain_id" || $value['Field'] == "domain_name" || $value['Field'] == "server_id" || $value['Field'] == "server_name" || $value['Field'] == "sogo_id" || $value['Field'] == "sys_userid" || $value['Field'] == "sys_groupid" || $value['Field'] == "sys_perm_user" || $value['Field'] == "sys_perm_group" || $value['Field'] == "sys_perm_other") {
+                continue;
+            }
+            $ret[$value['Field']] = array('name' => $value['Field'], 'default' => $value['Default']);
+        }
+        return $ret;
     }
 
 }
