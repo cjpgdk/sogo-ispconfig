073150e6f123e70a650d9ca3cb3e6519b253a621
Christian M. Jensen <cmjnisse@users.noreply.github.com>
2015-06-01 23:31:26 +0200

    Debian(8) Jessie, is now officially supported

--- _ins/SOGo.php
+++ _ins/SOGo.php
@@ -113,8 +113,6 @@ class SOGo {
             $this->os_name = strtolower(Installer::readInput("{$this->os_name}"));
             switch ($this->os_name) {
                 case 'jessie':
-                    return "deb http://inverse.ca/debian-nightly/ {$this->os_name} {$this->os_name}" . PHP_EOL . "#deb-src http://inverse.ca/debian-nightly/ {$this->os_name} {$this->os_name}";
-                    break;
                 case 'lenny':
                 case 'squeeze':
                 case 'wheezy':
