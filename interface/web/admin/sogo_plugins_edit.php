e9af81de7f9b0f2ab4279f534344c56e5823a0f2
Christian M. Jensen <cmjscripter@yahoo.com>
2015-06-23 11:37:48 +0200

    Merge testing

--- interface/web/admin/sogo_plugins_edit.php
+++ interface/web/admin/sogo_plugins_edit.php
@@ -42,11 +42,12 @@ $app->load('tform_actions');
 class tform_action extends tform_actions {
 
     private $upload_ok = FALSE;
+
     public function onShowEnd() {
         global $app, $conf;
         if ($this->dataRecord['filetype'] == "download" && !file_exists("{$conf['sogo_plugins_upload_dir']}/{$this->dataRecord['file']}")) {
-            $this->dataRecord['active']= 'n'; //* deactivate if file is not found
-            $app->tpl->setVar('error', sprintf($app->tform->wordbook['download_file_missing'], "{$conf['sogo_plugins_upload_dir']}/{$this->dataRecord['file']}"));
+            $this->dataRecord['active'] = 'n'; //* deactivate if file is not found
+            $app->tpl->setVar('error', sprintf($app->tform->wordbook['download_file_missing'], "{$conf['sogo_plugins_upload_dir']}/{$this->dataRecord['file']}") . "{$conf['sogo_plugins_upload_dir']}/{$this->dataRecord['file']}");
         }
         parent::onShowEnd();
     }
@@ -60,6 +61,7 @@ class tform_action extends tform_actions {
         global $app;
         parent::onAfterInsert();
         if ($this->upload_ok) {
+            $this->fixRecordPermissions();
             $_REQUEST["next_tab"] = 'plugins';
             $app->tpl->setVar('msg', str_replace(array('{LINK}', '{/LINK}'), array('<a href="#" onclick="loadContent(\'admin/sogo_plugins_list.php\');">', '</a>'), $app->tform->wordbook['all_good_return']));
         }
@@ -68,30 +70,39 @@ class tform_action extends tform_actions {
     public function onBeforeUpdate() {
         global $app, $conf;
         $this->upload_ok = $this->setFileName(true);
-        
         if ($this->dataRecord['filetype'] == "download" && !file_exists("{$conf['sogo_plugins_upload_dir']}/{$this->dataRecord['file']}")) {
-            $this->dataRecord['active']= 'n';
+            $this->dataRecord['active'] = 'n';
             $app->tpl->setVar('error', sprintf($app->tform->wordbook['download_file_missing'], "{$conf['sogo_plugins_upload_dir']}/{$this->dataRecord['file']}"));
         }
         parent::onBeforeUpdate();
     }
 
-    /**
-     * 
-     * @global app $app
-     */
     public function onAfterUpdate() {
         global $app;
         parent::onAfterUpdate();
         if ($this->upload_ok) {
+            $this->fixRecordPermissions();
             $_REQUEST["next_tab"] = 'plugins';
             $app->tpl->setVar('msg', str_replace(array('{LINK}', '{/LINK}'), array('<a href="#" onclick="loadContent(\'admin/sogo_plugins_list.php\');">', '</a>'), $app->tform->wordbook['all_good_return']));
         }
     }
 
+    private function fixRecordPermissions() {
+        global $app;
+        //* if client_id isset, set propper sys_* values, default is uploader with global read permissions
+        if ($this->dataRecord['client_id'] != 0) {
+            //* we simple just copy the data from sys_user table
+            $client_sys_data = $app->db->queryOneRecord("SELECT `userid`,`default_group`,`sys_perm_user`,`sys_perm_group`,`sys_perm_other` FROM `sys_user` WHERE client_id='{$app->db->quote($this->dataRecord['client_id'])}'");
+            if ($client_sys_data !== false)
+                $app->db->query("UPDATE `dbispconfig`.`sogo_plugins` SET "
+                        . " `sys_userid` = '{$app->db->quote($client_sys_data['userid'])}', `sys_groupid` = '{$app->db->quote($client_sys_data['default_group'])}', "
+                        . " `sys_perm_user` = '{$app->db->quote($client_sys_data['sys_perm_user'])}', `sys_perm_group` = '{$app->db->quote($client_sys_data['sys_perm_group'])}', "
+                        . " `sys_perm_other` = '{$app->db->quote($client_sys_data['id'])}' WHERE `spid` = '{$app->db->quote($this->dataRecord['id'])}';");
+        }
+    }
+
     //* upload file or set file name to http link
     private function setFileName($isupd = false) {
-
         global $app, $conf;
         if ($this->dataRecord['filetype'] == "download") {
             if (($isupd !== false && isset($_FILES['file_download']) && is_uploaded_file($_FILES['file_download']['tmp_name'])) || $isupd === false) {
@@ -117,10 +128,18 @@ class tform_action extends tform_actions {
                         $app->tform->errorMessage .= $app->lng("No input file selected for upload") . "<br>";
                     } else {
                         $file_name = basename($_FILES["file_download"]["name"]);
-                        if (file_exists($conf['sogo_plugins_upload_dir'] . "/" . $file_name))
+                        if (file_exists($conf['sogo_plugins_upload_dir'] . "/" . $file_name)) {
                             @unlink($conf['sogo_plugins_upload_dir'] . "/" . $file_name);
+                        }
                         if (move_uploaded_file($_FILES["file_download"]["tmp_name"], $conf['sogo_plugins_upload_dir'] . "/" . $file_name)) {
+                            $old_file_name = $this->dataRecord['file_http'];
                             $this->dataRecord['file'] = $file_name;
+                            //* if new filename unlink old file
+                            if ($old_file_name != $file_name) {
+                                if (file_exists($conf['sogo_plugins_upload_dir'] . "/" . $old_file_name))
+                                    @unlink($conf['sogo_plugins_upload_dir'] . "/" . $old_file_name);
+                                $_POST['file_http'] = $file_name;
+                            }
                         } else {
                             $app->tform->errorMessage .= $app->lng("Sorry, there was an error uploading your file.") . "<br>";
                         }
