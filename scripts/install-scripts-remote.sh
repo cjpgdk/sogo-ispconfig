8459e694df30a60a4f8630c182a891ab715618c7
Christian M. Jensen <cmjscripter@yahoo.com>
2015-06-28 12:36:54 +0200

    install in local bin

--- scripts/install-scripts-remote.sh
+++ scripts/install-scripts-remote.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-mkdir -p /usr/local/src/sogo-ispconfig/
+mkdir -p /usr/local/bin/
 
-cd /usr/local/src/sogo-ispconfig/
+cd /usr/local/bin/
 
-wget --no-check-certificate http://raw.githubusercontent.com/cmjnisse/sogo-ispconfig/master/scripts/sogo-ispconfig-checkout-master.sh -O /usr/local/src/sogo-ispconfig/sogo-ispconfig-checkout-master.sh
+wget --no-check-certificate http://raw.githubusercontent.com/cmjnisse/sogo-ispconfig/master/scripts/sogo-ispconfig-checkout-master.sh -O /usr/local/bin/sogo-ispconfig-checkout-master.sh
 
-wget --no-check-certificate http://raw.githubusercontent.com/cmjnisse/sogo-ispconfig/master/scripts/sogo-ispconfig-checkout-testing.sh -O /usr/local/src/sogo-ispconfig/sogo-ispconfig-checkout-testing.sh
+wget --no-check-certificate http://raw.githubusercontent.com/cmjnisse/sogo-ispconfig/master/scripts/sogo-ispconfig-checkout-testing.sh -O /usr/local/bin/sogo-ispconfig-checkout-testing.sh
 
-wget --no-check-certificate http://raw.githubusercontent.com/cmjnisse/sogo-ispconfig/master/scripts/sogo-ispconfig-clone.sh -O /usr/local/src/sogo-ispconfig/sogo-ispconfig-clone.sh
+wget --no-check-certificate http://raw.githubusercontent.com/cmjnisse/sogo-ispconfig/master/scripts/sogo-ispconfig-clone.sh -O /usr/local/bin/sogo-ispconfig-clone.sh
 
-wget --no-check-certificate http://raw.githubusercontent.com/cmjnisse/sogo-ispconfig/master/scripts/sogo-ispconfig-full-update.sh -O /usr/local/src/sogo-ispconfig/sogo-ispconfig-full-update.sh
+wget --no-check-certificate http://raw.githubusercontent.com/cmjnisse/sogo-ispconfig/master/scripts/sogo-ispconfig-full-update.sh -O /usr/local/bin/sogo-ispconfig-full-update.sh
 
-wget --no-check-certificate http://raw.githubusercontent.com/cmjnisse/sogo-ispconfig/master/scripts/sogo-ispconfig-pull.sh -O /usr/local/src/sogo-ispconfig/sogo-ispconfig-pull.sh
+wget --no-check-certificate http://raw.githubusercontent.com/cmjnisse/sogo-ispconfig/master/scripts/sogo-ispconfig-pull.sh -O /usr/local/bin/sogo-ispconfig-pull.sh
 
-wget --no-check-certificate http://raw.githubusercontent.com/cmjnisse/sogo-ispconfig/master/scripts/sogo-ispconfig-update.sh -O /usr/local/src/sogo-ispconfig/sogo-ispconfig-update.sh
+wget --no-check-certificate http://raw.githubusercontent.com/cmjnisse/sogo-ispconfig/master/scripts/sogo-ispconfig-update.sh -O /usr/local/bin/sogo-ispconfig-update.sh
 
-chmod +x -R /usr/local/src/sogo-ispconfig/
+chmod +x -R /usr/local/bin/sogo-*
