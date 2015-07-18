556de834afe986885c4b63b34282c4bc3d0484c2
Christian M. Jensen <cmjscripter@yahoo.com>
2015-07-18 15:47:53 +0200

    wrong txt field and missing sql

--- _ins/drop_tables.sql
+++ _ins/drop_tables.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS `sogo_config`;
 DROP TABLE IF EXISTS `sogo_domains`;
 DROP TABLE IF EXISTS `sogo_module`;
 DROP TABLE IF EXISTS `sogo_plugins`;
+DROP TABLE IF EXISTS `sogo_config_permissions`;
 DROP TABLE IF EXISTS `sogo_config_permissions_index`;
 
 DELETE FROM `sys_config` WHERE `group`='interface' AND `name`='sogo_interface';
