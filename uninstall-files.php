021ebd2e425bbe11edcd42ece570d3e2ba2ce1f8
Christian M. Jensen <christian@cmjscripter.net>
2015-07-29 11:44:07 +0200

    ref #32

---
+++ uninstall-files.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * delete all files related to this adon
+ * 
+  cd /tmp
+  wget https://github.com/cmjnisse/sogo-ispconfig/archive/master.tar.gz -O sogo-ispconfig.tar.gz
+  tar -xvf sogo-ispconfig.tar.gz
+  cd sogo-ispconfig-master
+  php uninstall-files.php
+ */
+
+require '_ins/copy_files.php';
+require '_ins/old_files.php';
+
+function readInput($default = "") {
+    $handle = fopen("php://stdin", "r");
+    $line = fgets($handle);
+    return (!empty($line) && trim($line) != "" ? trim($line) : $default);
+}
+
+echo PHP_EOL . PHP_EOL;
+
+echo "location of ISPConfig folder? [/usr/local/ispconfig]: ";
+$ispcdir = readInput("/usr/local/ispconfig");
+
+echo PHP_EOL . PHP_EOL;
+foreach ($old_files as $key => $value) {
+    if ($value['type'] == 'file')
+        @unlink($ispcdir . '/' . $value['file']);
+    else if ($value['type'] == 'folder')
+        echo "The folowing folder is not delete: " . $ispcdir . '/' . $value['folder'] . PHP_EOL;
+}
+
+foreach ($files_copy as $key => $value) {
+    foreach ($value as $value2) {
+        @unlink($ispcdir . '/' . $key . "/" . $value2);
+    }
+}
